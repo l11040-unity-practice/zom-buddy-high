b223781df433935e52cbcc137f3e27f990d72197
l11040 <113540683+l11040@users.noreply.github.com>
2024-04-02 17:58:55 +0900

    캐릭터 타일맵 안에서만 움직이게 하기

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,24 +5,39 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Animator _anime;
-
-    public float Speed = 5f;
+    Rigidbody2D _rb;
+    Vector2 _movement;
+    public float Speed = 3f;
 
     void Start()
     {
         _anime = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        float hInput = Input.GetAxis("Horizontal");
-        float vInput = Input.GetAxis("Vertical");
+        _movement.x = Input.GetAxis("Horizontal");
+        _movement.y = Input.GetAxis("Vertical");
+    }
 
-        Vector3 movement = new Vector2(hInput, vInput);
+    void FixedUpdate()
+    {
+        Vector2 newPos = _rb.position + _movement.normalized * Speed * Time.fixedDeltaTime;
+        _rb.MovePosition(newPos);
+        SetMoveAnimation();
+    }
 
-        transform.position += movement * Speed * Time.deltaTime;
+    void SetMoveAnimation()
+    {
+        bool isDiagonalMovement = Mathf.Abs(_movement.x) > 0.01f && Mathf.Abs(_movement.y) > 0.01f;
 
-        _anime.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
-        _anime.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        _anime.SetFloat("MoveX", _movement.x);
+        _anime.SetFloat("MoveY", _movement.y);
+
+        if (isDiagonalMovement)
+        {
+            _anime.SetFloat("MoveY", 0);
+        }
     }
 }
